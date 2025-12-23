94621a0cb76468c33321fd9ec55b1bdde158e36a
dufutao <futaodu5@gmail.com>
2025-12-23 15:23:14 +0800

    Add developer signature

--- EarthLord/ContentView.swift
+++ EarthLord/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Developed by taozi")
+                .font(.headline)
+                .foregroundStyle(.secondary)
         }
         .padding()
     }
